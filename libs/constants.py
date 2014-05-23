a18cec6470c4bb81c9385bad86d0bb9ecc61fd89
Alexander Gorokhov <sashgorokhov@gmail.com>
2014-05-23 21:04:24 +0600

    Added test group to group selection menu.

--- libs/constants.py
+++ libs/constants.py
@@ -3,9 +3,9 @@ __author__ = 'Alexander'
 application_id = '4148076'
 permissions_scope = ["groups", "wall", "photos"]
 groups = {
-    'ru9gag': '32041317', # official id = 32041317 test id = 66273726
+    'ru9gag': '32041317',
     '9gifs': '55326998',
     '9geek': '56910779',
     '9cute': '56910814',
-    '9nsfw': '61957890'}
-#print("Warning: dont forget to change ru9gag ID")
+    '9nsfw': '61957890',
+    '9GAG Test': '66273726'}
